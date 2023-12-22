8ab91c2559ab4438208d5bcd4e560901df670f01
sohan0898 <shuhanur.rahman08@gmail.com>
2023-12-22 17:14:29 +0600

    added upated, delete and get method for task

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
 const ports = process.env.PORT || 5000;
@@ -8,12 +10,111 @@ const ports = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// routes
+// MongoDB
 
-app.get('/', (req, res) => {
-res.send('TaskPedia Server Started');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8p2aqm7.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+
+    const taskCollection = client.db("taskpedia").collection("tasks");
+
+    // routes
+
+    // tasks api
+
+    app.get("/tasks", async (req, res) => {
+      const { email } = req.query;
+      const query = email ? { email } : {};
+      const result = await taskCollection.find(query).toArray();
+      console.log(result);
+      res.send(result);
+    });
+
+    app.get("/tasks/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await taskCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.post("/tasks", async (req, res) => {
+      const item = req.body;
+      const result = await taskCollection.insertOne(item);
+      res.send(result);
+    });
+
+    app.patch("/tasks/:id", async (req, res) => {
+      const items = req.body;
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          title: items.title,
+          prioritys: items.prioritys,
+          description: items.description,
+          deadline: items.deadline,
+        },
+      };
+      const result = await taskCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
+
+    app.delete("/tasks/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await taskCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("TaskPedia Server Started");
+});
+
+// check server api health
+
+app.get("/health", (req, res) => {
+  res.send("taskpedia is running....");
+});
+
+//error Handle for post, get
+
+app.all("*", (req, res, next) => {
+  const error = new Error(`Can't find ${req.originalUrl} on the server`);
+  error.status = 404;
+  next(error);
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    message: err.message,
+    errors: err.errors,
+  });
 });
 
 app.listen(ports, () => {
-console.log( `server running on port ${ports}`);
+  console.log(`server running on port ${ports}`);
 });
